127d441d44bb57f0e72c1ac89d1f8c5db0352a83
Ankit Agrawal <40805442+ankiitagrwal@users.noreply.github.com>
2026-06-05 07:44:55 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let start = 0 , last = numbers.length-1
+        while(start< last){
+            if(numbers[start] + numbers[last] > target) {
+                last--
+            }
+            else if(numbers[start] + numbers[last] < target) {
+                start++
+            }
+            else {
+                return [start +1, last +1]
+            }
+            
+            
+        }
+        return [start +1, last +1]
+    }
+}
