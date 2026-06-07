52357afdcdcf39ff4a6924fb02ee98f2942b3a91
Ankit Agrawal <40805442+ankiitagrwal@users.noreply.github.com>
2026-06-07 15:29:12 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let maxHeighth =0, left= 0, right= heights.length-1
+        while(left<right){
+            const value = Math.min(heights[left] , heights[right])
+            maxHeighth = Math.max(maxHeighth, value * (right-left))
+            if(heights[left] < heights[right]){
+                left++
+            }
+            else if (heights[left] > heights[right]){
+                right--
+            }
+            else{
+                left++
+               
+            }
+        }
+        return maxHeighth
+    }
+}
