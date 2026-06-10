03fbf221b171de0015dc68ea56244691ea17da4e
Ankit Agrawal <40805442+ankiitagrwal@users.noreply.github.com>
2026-06-10 07:44:23 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const newArray = Array.from({length :temperatures.length}).fill(0)
+        for(let i =0; i< temperatures.length-1; i++){
+            for(let j =i+1; j< temperatures.length; j++){
+                if(temperatures[j] > temperatures[i]){
+                    newArray[i]= j-i;
+                    break
+                }
+            }
+        }
+        return newArray
+    }
+}
