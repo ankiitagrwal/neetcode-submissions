8a8ab6711214d1afa9575785f8f78d58e97988d3
Ankit Agrawal <40805442+ankiitagrwal@users.noreply.github.com>
2026-07-01 09:41:45 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.js
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        const start =intervals.map(i => i.start).sort((a,b) => a - b);
+        const end = intervals.map(i => i.end).sort((a, b) => a - b);
+        let res =0, count =0, s=0, e=0;
+        while (s < start.length){
+            if (start[s]< end[e]){
+                count++;
+                s++
+            }else {
+                count--;
+                e++
+            }
+            res = Math.max(res, count)
+        }
+        return res
+    }
+}
